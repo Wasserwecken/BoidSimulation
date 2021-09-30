f6ae7759df3e5993ffb95896ae9e4f420ce31c42
Wasserwecken <eric.dolch@gmail.com>
2021-09-30 18:22:21 +0200

    removed project files

--- Assets/BoidSimulation/Scripts/Chunks/IChunkEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Numerics;
-
-public interface IChunkEntity
-{
-    void SetChunkManager<TChunk, TAggregation>(IChunkManager<TChunk, TAggregation> manager) where TChunk : IChunkEntity;
-    Vector3 ProvidePosition();
-}
